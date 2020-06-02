e6cbcf6e85b181d9f34531c0afa17560acef9077
Alex <format37@gmail.com>
2020-06-02 19:55:44 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ async def call_alice(request):
 	
 	#print('in',vars(request))
 	#print('in',dir(request.rel_url.query))
-	print('in',request.rel_url.query.keys())
+	print('in',len(request.rel_url.query.keys()))
 	content = "alice ok"	
 	return web.Response(text=content,content_type="text/html")
 	
