7f5ec60e9782dde42c76e34541f37e058e916038
Alex <format37@gmail.com>
2020-06-02 18:49:55 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
+
+PORT = '8443' #prod
+
 import asyncio
 from aiohttp import web
-import urllib
-import urllib.parse
-from urllib.parse import urlparse, parse_qsl
-import multidict as MultiDict
-import requests
+import ssl
 
 async def call_alice(request):
 	#request_id	= request.rel_url.query['request_id']
@@ -15,9 +14,15 @@ async def call_alice(request):
 app = web.Application()
 app.router.add_route('GET', '/alice', call_alice)
 
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain('fullchain.pem', 'privkey.pem')
+
+cred = credentials.Certificate("/home/format37_gmail_com/projects/langtea/backend/json/langtea-firebase-adminsdk-q4d01-b85fc8ed07.json")
+firebase_admin.initialize_app(cred)
+
 loop = asyncio.get_event_loop()
 handler = app.make_handler()
-f = loop.create_server(handler, port='8081')
+f = loop.create_server(handler, port='8081', ssl=ssl_context)
 srv = loop.run_until_complete(f)
 
 print('serving on', srv.sockets[0].getsockname())
