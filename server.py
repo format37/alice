8ba0deedcf5b78ce21d1da0873703f00f4e25df9
Alex <format37@gmail.com>
2020-06-02 23:12:57 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ import ssl
 import firebase_admin
 from firebase_admin import credentials
 
+async def call_check(request):
+	return web.Response(text='ok',content_type="text/html")
+
 async def call_alice(request):
 	data = await request.json()
 	request	= data['request']
@@ -21,16 +24,20 @@ async def call_alice(request):
 	return web.json_response(response)
 
 app = web.Application()
+app.router.add_route('GET', '/check', call_check)
 app.router.add_route('POST', '/alice', call_alice, expect_handler = web.Request.json)
+
 ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_context.load_cert_chain('cert/fullchain.pem', 'cert/privkey.pem')
 cred = credentials.Certificate("cert/cert.json")
 firebase_admin.initialize_app(cred)
+
 loop = asyncio.get_event_loop()
 handler = app.make_handler()
 f = loop.create_server(handler, port=PORT, ssl=ssl_context)
 srv = loop.run_until_complete(f)
 print('serving on', srv.sockets[0].getsockname())
+
 try:
 	loop.run_forever()
 except KeyboardInterrupt:
