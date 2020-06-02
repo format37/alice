66898de88035c0193640c79876075ac97f2a1b47
Alex <format37@gmail.com>
2020-06-02 22:12:09 +0300

    deleted:    log.txt 	modified:   server.py

--- server.py
+++ server.py
@@ -7,32 +7,34 @@ from aiohttp import web
 import ssl
 import firebase_admin
 from firebase_admin import credentials
-	
-async def call_alice(request):	
-	
+
+async def call_alice(request):
 	data = await request.json()
-	
 	request	= data['request']
-	command	= request['command']
-	
-	print(command)
-	
-	content = "alice ok"
-	return web.Response(text=content,content_type="text/html")
+	command	= request['command']	
+	response = {
+        "version": data['version'],
+        "session": data['session'],
+        "response": {
+            "end_session": False
+        }
+    }		
+	response['response']['text']='command'
+	#content = "alice ok"
+	#return web.Response(text=response,content_type="text/html")
+	web.json_response(response)
+	#web.json_response({'error': message})
 
 app = web.Application()
 app.router.add_route('POST', '/alice', call_alice, expect_handler = web.Request.json)
-
 ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_context.load_cert_chain('cert/fullchain.pem', 'cert/privkey.pem')
 cred = credentials.Certificate("cert/cert.json")
 firebase_admin.initialize_app(cred)
-
 loop = asyncio.get_event_loop()
 handler = app.make_handler()
 f = loop.create_server(handler, port=PORT, ssl=ssl_context)
 srv = loop.run_until_complete(f)
-
 print('serving on', srv.sockets[0].getsockname())
 try:
 	loop.run_forever()
